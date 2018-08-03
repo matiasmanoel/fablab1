028ed3328b0e91a69c24593d07fff01007777352
willoliver21 <33660533+willoliver21@users.noreply.github.com>
2018-08-03 18:31:15 -0300

    Revert "Revert "NA""
    This reverts commit 1d55fefd9d484ee00b4398bbba46ac38ad2387b3.

--- resources/views/agendamento/create.blade.php
+++ resources/views/agendamento/create.blade.php
@@ -1,31 +1,43 @@
 @extends('layouts/app')
 @section('content')
 
+<div class="container">
+  <p>
+      <a href="{{'/agendamento'}}"style="color:#343a40"><< Voltar </a>
+  </p>
+    <div class="row justify-content-center">
     <h1>Agendamento</h1>
+    </div>
 
     {!! Form::model($agendamento, ['route' => 'agendamento.store'])  !!}
 
       <div class="form-group">
           {{ Form::label('Descrição', null, ['class' => 'control-label']) }}
           {{ Form::text('descricao', null, ['placeholder' => 'Preencha a Descrição', 'class' => 'form-control']) }}
+          <br>
 
           <!-- {{ Form::label('Horario', null, ['class' => 'control-label']) }}
           {{ Form::text('horario', null, ['placeholder' => 'Preencha o Horario', 'class' => 'form-control']) }} -->
 
           {!! Form::label('Horario', '* Digite o Horario: ') !!}
           {!! Form::time('horario',null, ['class' => 'form-control']) !!}
+          <br>
 
           {{ Form::label('Data', null, ['class' => 'control-label']) }}
           {{ Form::date('data', \Carbon\Carbon::today(), ['class' => 'form-control']) }}
+          <br>
 
           <!-- Mudar depois e colocar pra inserir id o usuario automaticamente como requerente -->
-          <!-- {{ Form::label('Requerente**', null, ['class' => 'control-label']) }} -->
-          <!-- {{ Form::text('requerente', null, ['placeholder' => 'Preencha o Requerente **', 'class' => 'form-control']) }} -->
-          <input name="requerente" type="hidden" value="{{Auth::user()->id}}">
+          <!-- {{ Form::label('Requerente**', null, ['class' => 'control-label']) }}
+          {{ Form::text('requerente', null, ['placeholder' => 'Preencha o Requerente **', 'class' => 'form-control']) }} -->
+          
       </div>
-
-      {!! Form::submit("Salvar", ['class' => 'btn btn-primary']) !!}
-
-    {!! Form::close() !!}
-
+      <br>
+          <div class="row justify-content-center">
+            <div class="col-1">
+              {!! Form::submit("Salvar", ['class' => 'btn btn-dark']) !!}
+              {!! Form::close() !!}
+            </div>
+          </div>
+</div>
 @endsection
--- resources/views/agendamento/edit.blade.php
+++ resources/views/agendamento/edit.blade.php
@@ -1,20 +1,34 @@
 @extends('layouts/app')
 @section('content')
+
+<div class="container">
+  <p>
+      <a href="{{'/agendamento'}}"style="color:#343a40"><< Voltar </a>
+  </p>
+    <div class="row justify-content-center">
     <h1>Editar Agendamento</h1>
+    </div>
     {!! Form::model($agendamento, ['method' => 'put', 'route' => ['agendamento.update', $agendamento]])  !!}
       <div class="form-group">
           {{ Form::label('Descrição', null, ['class' => 'control-label']) }}
           {{ Form::text('descricao', $agendamento->descricao, ['class' => 'form-control']) }}
+          <br>
 
           {{ Form::label('Horario', null, ['class' => 'control-label']) }}
           {{ Form::text('horario', $agendamento->horario, ['class' => 'form-control']) }}
+          <br>
 
           {{ Form::label('Data', null, ['class' => 'control-label']) }}
           {{ Form::text('data', $agendamento->data, ['class' => 'form-control']) }}
-
-          {{ Form::label('Requerente', null, ['class' => 'control-label']) }}
-          {{ Form::text('requerente', $agendamento->requerente, ['class' => 'form-control']) }}}
+          <br>
+          <!-- {{ Form::label('Requerente', null, ['class' => 'control-label']) }}
+          {{ Form::text('requerente', $agendamento->requerente, ['class' => 'form-control']) }} -->
       </div>
-      {!! Form::submit("Atualizar", ['class' => 'btn btn-primary']) !!}
-    {!! Form::close() !!}
+      <div class="row justify-content-center">
+        <div class="col-1">
+          {!! Form::submit("Atualizar", ['class' => 'btn btn-dark']) !!}
+          {!! Form::close() !!}
+        </div>
+      </div>
+</div>
 @endsection
