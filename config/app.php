364f60d6861a0760e9d1b188592ba01eb7bfa88b
willoliver21 <33660533+willoliver21@users.noreply.github.com>
2018-08-03 16:20:29 -0300

    Validações

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        LaravelLegends\PtBrValidator\ValidatorProvider::class,
 
 
         /*
